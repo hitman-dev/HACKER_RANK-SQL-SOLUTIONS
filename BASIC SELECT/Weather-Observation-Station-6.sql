2c93bcba89d54d05a4df28af8e4f28f8b6e9cdde
hitman-dev <72800256+hitman-dev@users.noreply.github.com>
2021-11-19 20:17:31 +0530

    Add files via upload

---
+++ BASIC SELECT/Weather-Observation-Station-6.sql
@@ -0,0 +1,10 @@
+select distinct(city) from station where
+city like "a%" or
+city like "e%" or
+city like "i%" or
+city like "o%" or
+city like "u%";
+
+# OR
+
+select distinct city from station where left(city,1) in('a','e','i','o','u')
