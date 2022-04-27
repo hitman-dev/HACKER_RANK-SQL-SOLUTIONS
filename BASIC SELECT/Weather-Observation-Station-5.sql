5510df16f61f95a7a4966164f74c15d1b3f4adf8
HITESH KISHOR CHAUDHARI <72800256+hitman-dev@users.noreply.github.com>
2022-04-27 20:09:19 +0530

    Update Weather-Observation-Station-5.sql

--- BASIC SELECT/Weather-Observation-Station-5.sql
+++ BASIC SELECT/Weather-Observation-Station-5.sql
@@ -1,2 +1,4 @@
 select city,length(city) from station order By length(city) asc, city asc limit 1;
 select distinct(City),length(city) from station order by length(city) desc, city asc limit 1;
+
+/* limit 1 means select only one row*/
